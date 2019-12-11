91adba11858d637211158a39a83d6fa2d61f0a4c
yzgarrard <yzgarrard@gmail.com>
2019-12-11 16:36:57 -0700

    Correct PD controller

--- src/Main.py
+++ src/Main.py
@@ -28,9 +28,9 @@ gd = {
     'delta_f_u_y': 0.0,
     'delta_f_v_z': 0.0,
     'delta_f_delta_x': 0.0,
-    'A_exp': 1225,  # m or cm or px?
-    'alpha_u': 100,
-    'alpha_v': 100,
+    'A_exp': .1225,  # m or cm or px?
+    'alpha_u': 214,  # px per m at target   Should actually be 214
+    'alpha_v': 214,  # px per m at target   Should actually be 214
     'd_exp': 1,
     'FOV_u': 90.0,
     'FOV_v': "",
@@ -49,7 +49,6 @@ gd = {
     'pitch_angle': 0.0,
     'roll_angle': 0.0,
     'yaw_angle': 90.0,
-    'got_initial_frame': False,
     'f_u_ref': 0.5,
     'f_v_ref': 0.5,
     'f_delta_ref': 17,
@@ -57,10 +56,10 @@ gd = {
     'kd_vx': 0.0124,
     'kp_vy': 0.298,
     'kd_vy': 0.145,
-    'kp_yaw': 1.990,
+    'kp_yaw': 0.990,# 'kp_yaw': 1.990,
     'kd_yaw': 0.119,
-    'kp_vz': 0.730,
-    'kd_vz': 0.171,
+    'kp_vz': 1.430,# 'kp_vz': 0.730,
+    'kd_vz': 0.371,#'kd_vz': 0.171,
     'camera_period': 1/30,
     'controller_period': 1/100,
     'local_x': 0,
@@ -239,7 +238,7 @@ def _decouplecentroiddata():
     if math.fabs(gd['psi_telem_ref'] - gd['yaw_angle']) > 25:
         gd['yaw_angle'] = gd['yaw_angle']
     delta_f_u_psi = f_u - initial_f_u
-    delta_f_u_y = delta_f_u_psi # - ((gd['psi_telem_ref'] - gd['yaw_angle']) / FOV_u)
+    delta_f_u_y = delta_f_u_psi - ((gd['psi_telem_ref'] - gd['yaw_angle']) / FOV_u)
     delta_f_v_z = (f_v - initial_f_v) - ((theta_centroid_ref - gd['pitch_angle']) / FOV_v)
     delta_f_delta_x = f_delta - initial_f_delta
 
@@ -293,22 +292,22 @@ def getsetpoints():
 
     # x velocity controller
     delta_x_tme = delta_f_delta_x * math.sqrt(A_exp) * math.sqrt((alpha_u * alpha_v) / (w_im * h_im))
-    v_xr = delta_x_tme * kp_vx + ((delta_x_tme - prev_delta_x_tme) * (controller_period)) * kd_vx
+    v_xr = delta_x_tme * kp_vx + ((delta_x_tme - prev_delta_x_tme) / (controller_period)) * kd_vx
     prev_delta_x_tme = delta_x_tme
 
     # y velocity controller
     delta_y_tme = delta_f_u_y * d_exp * (w_im / alpha_u)
-    v_yr = delta_y_tme * kp_vy + ((delta_y_tme - prev_delta_y_tme) * (controller_period)) * kd_vy
+    v_yr = delta_y_tme * kp_vy + ((delta_y_tme - prev_delta_y_tme) / (controller_period)) * kd_vy
     prev_delta_y_tme = delta_y_tme
 
     # yawrate controller
     delta_psi_tme = delta_f_u_psi * FOV_u
-    yawrate = delta_psi_tme * kp_yaw + ((delta_psi_tme - prev_delta_psi_tme) * (controller_period)) * kd_yaw
+    yawrate = delta_psi_tme * kp_yaw + ((delta_psi_tme - prev_delta_psi_tme) / (controller_period)) * kd_yaw
     prev_delta_psi_tme = delta_psi_tme
 
     # z velocity controller
     delta_z_tme = delta_f_v_z * d_exp * (h_im / alpha_v)
-    v_zr = delta_z_tme * kp_vz + ((delta_z_tme - prev_delta_z_tme) * (controller_period)) * kd_vz
+    v_zr = delta_z_tme * kp_vz + ((delta_z_tme - prev_delta_z_tme) / (controller_period)) * kd_vz
     prev_delta_z_tme = delta_z_tme
 
     gd['delta_x_tme'] = delta_x_tme
